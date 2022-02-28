13a9b2c2b1fce64daf7d7d2d6e1258110d0aeba8
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-02-28 09:49:39 +0600

    load data more

--- api.js
+++ api.js
@@ -5,7 +5,23 @@ function loadData(){
 
 fetch('https://jsonplaceholder.typicode.com/todos/1')
 .then(response => response.json())
-.then(json => console.log(json))
+.then(json => console.log(json));
+}
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data));
+    // .then(data => console.log(data));
+}
+
+function loadPost(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
+function displayUsers(data){
+    console.log(data);
+
 }
 
 
